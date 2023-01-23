c68140ac32ad3e064630e1d07b671373930f6206
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-01-23 16:51:15 +0100

    deleted obj

--- obj/src/app.cpp.d
+++
@@ -1,5 +0,0 @@
-obj/./src/app.cpp.o: src/app.cpp src/app.h src/io/readFlags.h
-
-src/app.h:
-
-src/io/readFlags.h:
--- obj/src/io/readFlags.cpp.d
+++
@@ -1,3 +0,0 @@
-obj/./src/io/readFlags.cpp.o: src/io/readFlags.cpp src/io/readFlags.h
-
-src/io/readFlags.h:
